7dab00f78caa557cb034ec56c56d5c9c60536fec
zKjet <100480520+zkjet@users.noreply.github.com>
2022-08-14 07:14:03 -0600

    adjusted static files directory correctly

--- scanny/settings.py
+++ scanny/settings.py
@@ -113,8 +113,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
